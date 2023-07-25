a51b7c2839420620f67e2c481fdffddb29903e2e
digitalready-victorl <140425493+digitalready-victorl@users.noreply.github.com>
2023-07-25 16:04:46 -0400

    Add files via upload

---
+++ cards-main/my_card_game.py
@@ -0,0 +1,17 @@
+# Rename this file to the name of your game and delete this comment
+# Names: 
+# Date: 
+
+# Import statements
+from card import Card
+
+# Program your game here!
+# Rename this function to the name of your game and delete this comment
+def my_card_game():
+    deck = Card.new_deck()
+    print(deck)
+
+# Code that runs when script is called from terminal
+# ex: python my_card_game.py
+if __name__ == "__main__":
+    my_card_game()
